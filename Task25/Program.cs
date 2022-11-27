4835e41e364e6e79445219545466e0055a8ebea1
Maria <mineeva_maria@mail.ru>
2022-11-27 13:38:42 +0300

    Solved

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int GetPower(int numA, int numB)
+{
+   int result = 1;
+   for(int i = 0; i < numB; i++)
+   {
+    result = numA * result;
+   }
+    return result;
+}
+Console.WriteLine(GetPower(3, 5));
